c1bbf038e50b82f0aaaea3dd02db39a733d7bfa9
bgens <brentpgensemer@gmail.com>
2019-04-04 07:08:04 -0700

    Update ctlenum.py
    added -nw option (no wildchard subdomains)
    
    fixed output print
    
    deduped output

--- ctlenum.py
+++ ctlenum.py
@@ -4,15 +4,18 @@ import config
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--target', help="target for scan. example: --target google.com", required=True)
+parser.add_argument('-nw', action='store_true', help="target for scan. example: --target google.com")
+
 args = parser.parse_args()
 
 target = args.target
 
 
 class CtlEnum(object):
-    def __init__(self, api, domain):
+    def __init__(self, api, domain, nowilds):
         self.api = api
         self.domain = domain
+        self.nowilds = nowilds
 
     def print_target(self):
         print('Target Domain: ', str(self.domain))
@@ -28,14 +31,21 @@ class CtlEnum(object):
             list_count = len(item['dns_names'])
             if list_count > 1:
                 for entry in item['dns_names']:
-                    dns_list.append(str(entry))
+                    if self.nowilds is True and entry[0][:1] is '*':
+                        pass
+                    else:
+                        dns_list.append(str(entry))
             else:
                 str_item = str(item['dns_names'][0])
-                dns_list.append(str_item)
+                if self.nowilds is True and str_item[0][:1] is '*':
+                    pass
+                else:
+                    dns_list.append(str_item)
 
-        return dns_list
+        return set(dns_list)
 
 
-scan = CtlEnum(config.api_key, target)
+scan = CtlEnum(config.api_key, target, args.nw)
 
-print(scan.get_dns())
+for dnsentry in scan.get_dns():
+    print(dnsentry)
