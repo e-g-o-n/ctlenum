2bd84f36a3b3e0cc040e1e82bbe17caf765e170f
bgens <brentpgensemer@gmail.com>
2019-04-08 19:11:48 -0700

    Argument changes
    Made massive changes to arguments.
    By default, now hides wildcard domains & exclusively shows target domain output.
    
    Removed -nw and -e
    
    Added -a and -w

--- ctlenum.py
+++ ctlenum.py
@@ -4,9 +4,9 @@ import config
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--target', help="target for scan. example: --target google.com", required=True)
-parser.add_argument('-nw', action='store_true', help="no wildcards.  example: ignores *.google.com")
-parser.add_argument('-e', action='store_true', help="exclusive output. example: if target is google.com,"
-                                                    " only show google.com domains")
+parser.add_argument('-w', action='store_true', help="Wildcards.  Shows wildcard subdomains like: *.google.com")
+parser.add_argument('-a', action='store_true', help="All output. Show non-target domains caught in search."
+                                                    " Useful for finding vendors for target domain")
 
 
 args = parser.parse_args()
@@ -15,18 +15,19 @@ target = args.target
 
 
 class CtlEnum(object):
-    def __init__(self, api, domain, nowilds):
+    def __init__(self, api, domain, wild):
         self.api = api
         self.domain = domain
-        self.nowilds = nowilds
+        self.wild = wild
 
     def print_target(self):
         print('Target Domain: ', str(self.domain))
 
     def get_dns(self):
-        url = ['https://api.certspotter.com/v1/issuances?domain=', self.domain,\
-              '&include_subdomains=true&match_wildcards=true&expand=dns_names']
-        header = {'Authorization': 'Bearer ' + self.api}
+        url = ['https://api.certspotter.com/v1/issuances?domain=', self.domain,
+               '&include_subdomains=true&match_wildcards=true&expand=dns_names']
+        bearer = ['Bearer ', self.api]
+        header = {'Authorization': ''.join(bearer)}
         r = requests.get(''.join(url), headers=header).json()
         dns_list = list()
 
@@ -34,13 +35,13 @@ class CtlEnum(object):
             list_count = len(item['dns_names'])
             if list_count > 1:
                 for entry in item['dns_names']:
-                    if self.nowilds is True and entry[0][:1] == '*':
+                    if self.wild is False and entry[0][:1] == '*':
                         pass
                     else:
                         dns_list.append(str(entry))
             else:
                 str_item = str(item['dns_names'][0])
-                if self.nowilds is True and str_item[0][:1] == '*':
+                if self.wild is False and str_item[0][:1] == '*':
                     pass
                 else:
                     dns_list.append(str_item)
@@ -48,14 +49,13 @@ class CtlEnum(object):
         return set(dns_list)
 
 
-scan = CtlEnum(config.api_key, target, args.nw)
+scan = CtlEnum(config.api_key, target, args.w)
 
 if __name__ == '__main__':
-    if args.e is True:
+    if args.a is False:
         for dnsentry in scan.get_dns():
             if target in dnsentry:
                 print(dnsentry)
     else:
         for dnsentry in scan.get_dns():
             print(dnsentry)
-
